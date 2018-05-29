4f8ded0838d6d16e4f018ec998dfef9214d06e1e
jordanvolz <jordan.volz@cloudera.com>
2018-05-29 15:32:52 -0400

    Update cdsw-build.sh

--- cdsw-build.sh
+++ cdsw-build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-rm -r -f models/spark/*
-tar -xf models/spark_rf.tar
+#!/bin/bash
+
+if [ -d "models/spark/" ] 
+then
+  rm -r -f models/spark/*
+  if [ -f "models/spark_rf.tar"]
+  then tar -xf models/spark_rf.tar 
+  fi
+fi
+
